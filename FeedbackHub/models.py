79e40180d9921c781d45db335bab1a0fbd094893
FELIPE <felipe3133770868@gmail.com>
2024-04-29 18:40:19 -0500

    feat: Agregar modelos para la aplicacion FeedbackHub (FeedbackHub/models.py)

--- FeedbackHub/models.py
+++ FeedbackHub/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class TipoMensaje(models.Model):
+    tipo = models.CharField(max_length=11, unique=True)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=150)
+    correo_electronico = models.EmailField()
+    
+    class Meta:
+        unique_together = [['nombre', 'correo_electronico']]
+
+class Mensaje(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    tipo = models.ForeignKey(TipoMensaje, on_delete=models.CASCADE)
+    asunto = models.CharField(max_length=250)
+    contenido = models.TextField()
+    fecha = models.DateField(auto_now=True)
+    hora = models.TimeField(auto_now=True)
