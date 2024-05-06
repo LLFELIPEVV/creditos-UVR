a07f62946b658e1f69b76fda3193c7dff3f926e9
FELIPE <felipe3133770868@gmail.com>
2024-05-05 20:25:57 -0500

    feat: Agregar frontend del UVRinfo

--- creditos_uvr/urls.py
+++ creditos_uvr/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from UVRinfo.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='inicio'),
     path('FeedbackHub', include('FeedbackHub.urls')),
     path('Simulador', include('Simulador.urls')),
 ]
